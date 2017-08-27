07b22755f2f68c568b0e9381afa63864b35d991b
Derek Honerlaw <derekhonerlaw@Dereks-MacBook-Pro.local>
2017-08-27 17:33:49 -0400

    update tests, add code coverage

--- typescript/test/lib/param.spec.ts
+++ typescript/test/lib/param.spec.ts
@@ -2,7 +2,8 @@ import * as chai from "chai";
 import "mocha";
 import "reflect-metadata";
 import {
-    ParseFunction, RequestBody, RequestContext, RequestContextValue, RequestEvent, RequestEventValue, RequestParam,
+    ParseFunction, RequestBody, RequestContext, RequestContextValue, RequestEvent, RequestEventValue,
+    RequestHeaderValue, RequestParam,
     RequestPath
 } from "../../lib/param";
 import {MetadataKey} from "../../lib/service";
@@ -32,6 +33,9 @@ describe("Param", () => {
                 break;
             case "context_value":
                 RequestContextValue(name, parse)(target, propertyKey, descriptor);
+                break;
+            case "header_value":
+                RequestHeaderValue(name, parse)(target, propertyKey, descriptor);
                 break;
             case "event":
                 RequestEvent()(target, propertyKey, descriptor);
@@ -66,8 +70,39 @@ describe("Param", () => {
         validate("path");
         validate("event_value");
         validate("context_value");
+        validate("header_value");
         validate("event", true);
         validate("context", true);
+    });
+
+    it("should append metadata with existing metadata", () => {
+        const propertyKey: string = "test";
+        const name: string = "test-name";
+        const nameTwo: string = "test-name-two";
+
+        RequestParam(name)(target, propertyKey, 1);
+        RequestParam(nameTwo)(target, propertyKey, 2);
+
+        chai.expect(Reflect.hasOwnMetadata(MetadataKey.PARAM, target.constructor)).to.be.true; // tslint:disable-line
+        chai.expect(Reflect.getOwnMetadata(MetadataKey.PARAM, target.constructor)).to.deep.equal([{
+            data: {
+                type: "param",
+                name,
+                parse: undefined
+            },
+            target,
+            propertyKey,
+            descriptor: 1
+        }, {
+            data: {
+                type: "param",
+                name: nameTwo,
+                parse: undefined
+            },
+            target,
+            propertyKey,
+            descriptor: 2
+        }]);
     });
 
 });
